ae34089b1b9b361afe5c412b2448a988e78cfec5
Douglas Moura <douglasllmoura@gmail.com>
2015-07-29 23:26:18 -0300

    Refatorando quebra-cabecas

--- assets/Scripts/Jogos/Quebra-cabeca/Elementos.js
+++ assets/Scripts/Jogos/Quebra-cabeca/Elementos.js
@@ -1,12 +1,23 @@
 ﻿#pragma strict
 
 public var pecas: GameObject[];
-
+public var teste: GUITexture;
 
 function Start () {
+	//var go: GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	//var textures: Texture2D[] = Resources.LoadAll("\assets\Imagens\Quebra-cabeça\Tartaruga\Nivel 1\1_1.png") as Texture2D[];
+	//var texture: Texture2D = textures[Random.Range(0, textures.Length)];
+	//go.GetComponent.<Renderer>().material.mainTexture = texture;
+	//var www = new WWW("file:///assets/Imagens/Quebra-cabeça/Tartaruga/Nivel 1/1_1.png");
+
 	
 	for (var i in range(0, pecas.Length)) {
 		Instantiate(pecas[i], pecas[i].transform.position, Quaternion.identity);
+		var myFruit = Resources.Load("Tartaruga/1/" + (i + 1), typeof(Sprite)) as Sprite;
+		//pecas[i].renderer.material.mainTexture = www.texture;
+		pecas[i].GetComponent(SpriteRenderer).sprite = myFruit;
+		
+		
 		/**
 		var obj = Instantiate(pecas[i], pecas[i].transform.position, Quaternion.identity);
 		obj.renderer.material.color = Color.black;
