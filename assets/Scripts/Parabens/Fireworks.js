41eecee9ffa3d4c245f7ad72d615ccf0b1c7afb8
Douglas Moura <douglasllmoura@gmail.com>
2015-08-27 19:28:41 -0300

    Tela de parabens

---
+++ assets/Scripts/Parabens/Fireworks.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public var fogos1: GameObject;
+public var fogos2: GameObject;
+public var x: float[];
+public var y: float[];
+
+function Start () {
+	Criar();
+}
+
+function Update () {
+	
+}
+
+function Criar() {
+	for (var i = 0; i < 5; i++) {
+		Instantiate(fogos1, Vector3(Random.Range(-6.0, 6.0), Random.Range(2.0, 4.0), 2), Quaternion.identity);
+		yield WaitForSeconds(1.0);
+		Instantiate(fogos2, Vector3(Random.Range(-6.0, 6.0), Random.Range(2.0, 4.0), 2), Quaternion.identity);
+		yield WaitForSeconds(0.8);
+	}
+}
